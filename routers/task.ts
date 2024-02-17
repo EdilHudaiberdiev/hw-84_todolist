2584830cef1e713e40949d710e20e2d1b0f761ab
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2024-02-17 18:25:13 +0600

    added update & delete tasks

--- routers/task.ts
+++ routers/task.ts
@@ -30,9 +30,52 @@ taskRouter.get('/', auth, async (req: RequestWithUser, res, next) => {
   try {
     const task = await Task.find();
     return res.send(task);
-  } catch (e) {
-    next(e);
+
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res.status(422).send(error);
+    }
+    next(error);
   }
 });
+
+taskRouter.delete('/:id', auth, async (req: RequestWithUser, res, next) => {
+
+  try {
+    if (!req.params.id) {
+      res.status(400).send({"error": "Id params must be in url"});
+    }
+    await Task.deleteOne({_id: req.params.id});
+
+    return res.send("task was deleted");
+
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res.status(422).send(error);
+    }
+    next(error);
+  }
+
+});
+
+taskRouter.put('/:id', auth, async (req: RequestWithUser, res, next) => {
+
+  try {
+    if (!req.params.id) {
+      res.status(400).send({"error": "Id params must be in url"});
+    }
+    await Task.updateOne({_id: req.params.id}, req.body);
+
+    return res.send('task updated');
+
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res.status(422).send(error);
+    }
+    next(error);
+  }
+
+});
+
 
 export default taskRouter
